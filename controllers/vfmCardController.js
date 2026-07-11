3be85cafe80ca5cdf2403d439fe07b8dce59f475
Saif Ur Rehman <saiffemalik@gmail.com>
2026-07-11 03:02:43 +0100

    feat: setting initial skelton for VFM controller

---
+++ controllers/vfmCardController.js
@@ -0,0 +1,10 @@
+/**
+ * Controller: VFM Card Management
+ * Functionality: Handles corporate reward and grocery allowance card actions.
+ */
+const db = require('../config/db');
+
+// Placeholder for Create Operation (Next Step)
+exports.createCard = async (req, res) => {
+    res.send('VFM Card Controller - Create Operation Pending');
+};
